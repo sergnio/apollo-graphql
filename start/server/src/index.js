dfe489e7916b857c73e49502eada1029a1030d2a
Sergnio <sergnio@gmail.com>
2020-07-26 09:32:02 -0500

    Added: graphql mutations new db connection endpoints

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,24 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const { createStore } = require('./utils');
+
+const typeDefs = require('./schema');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+    typeDefs,
+
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
